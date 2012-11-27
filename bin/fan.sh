b7a5b4c70c0d38f976421907d1bf2d619c0e9991
Mike Shade <mshade@mshade.org>
2012-11-27 10:05:41 -0500

    added fan ctl script for MBP

---
+++ bin/fan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+
+# Set to manual control
+echo 1 | sudo tee /sys/devices/platform/applesmc.768/fan1_manual > /dev/null
+
+# Read fan rpm from input and echo to /sys file.
+
+echo $1 |sudo tee /sys/devices/platform/applesmc.768/fan1_output
