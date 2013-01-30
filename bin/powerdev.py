e85f4f136525b630bc7bc7c33ad1c6ac2b89f7d4
Mike Shade <mshade@mshade.org>
2013-01-30 15:25:08 -0500

    enhancements

--- bin/powerdev.py
+++ bin/powerdev.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python2
+# 
+# Control ponymix, a command line pulseaudio mixer, with the 
+# Griffin PowerMate.  
+# Requires: python-evdev module, ponymix, a PowerMate
 
-from evdev import InputDevice, ecodes
+
+from evdev import InputDevice, ecodes, list_devices
 from subprocess import call
 
-dev = InputDevice('/dev/input/event5')
-
+# Program to call for mixer operations
 mixer = "/usr/bin/ponymix"
+
+# Set device empty to start with - detect later, bail if not found.
+dev = None
+
+def findDevice():
+    # Map all accessible /dev/input devices
+    devices = map(InputDevice, list_devices())
+
+    for candidate in devices:
+        # Check for PowerMate in the device name string
+        if "PowerMate" in candidate.name: 
+            global dev 
+            dev = InputDevice(candidate.fn)
 
 def volAdjust(amt):
     vol_increase = [ mixer, "increase"]
@@ -22,10 +39,20 @@ def volMute():
     vol_mute = [ mixer, "toggle"]
     call(vol_mute)
 
+
+# Find the device
+findDevice()
+
+if dev is None:
+    print "No devices found.  Is PowerMate plugged in?"
+    quit()
+
+# Open the device and start polling for events.
 for event in dev.read_loop():
     # Knob turned - adjust volume up or down.
     if event.type == ecodes.EV_REL:
         volAdjust(event.value)
 
+    # Knob pressed down - call mute toggle
     elif event.type == ecodes.EV_KEY and event.value == 01:
         volMute()
