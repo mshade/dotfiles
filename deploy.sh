cfc565192e6c9f539ec2aad7a452a0ef9b43f6c6
Mike Shade <mshade@mshade.org>
2013-07-01 16:28:22 -0400

    refactored deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,36 +2,41 @@
 #
 # Script to deploy dotfiles repo to ~
 
-# Enumerate all tracked files.
-files=$(git ls-files)
 repodir="dotfiles"
 destination=$HOME
 
-# loop over files and create symlinks/directories where necessary
-cd $destination
+# directories to link explicitly
+DIRS="bin .vim"
+files=$(git ls-files |grep -v ^bin|grep -v ^.vim)
 
-for file in $files
+for dir in $DIRS
 do
-    # Test if file exists
-    if [[ -h $file ]]
+    if [[ -f $destination/$dir || -h $destination/$dir || -d $destination/$dir ]]
     then
-        echo "replacing $file with $repodir/$file"
-        rm -vf $destination/$file || exit 1
-        ln -s ~/$repodir/$file $destination/$file
-    elif [[ -f $file ]]
-    then
-        echo "replacing $file with $repodir/$file"
-        rm -vf $destination/$file || exit 1
-        ln -s ~/$repodir/$file $destination/$file
-    elif [[ -d $file ]]
-    then
-        echo "$file is a directory..."
+        echo "File exists, removing and linking."
+        rm -vf $destination/$dir || (echo "Deal with $dir yourself first, and then rerun." ; exit 1 )
+        ln -vs $destination/$repodir/$dir $destination/$dir
     else
-        echo "creating link to new $repodir/$file"
-        ln -s $destination/$repodir/$file $destination/$file || \
-            mkdir -p $destination/$(dirname $file) && \
-            ln -s $destination/$repodir/$file $destination/$file 
+        echo "Creating as new"
+        ln -vs $destination/$repodir/$dir $destination/$dir
     fi
 done
 
-
+for file in $files
+do
+    if [[ -f $destination/$file || -h $destination/$file ]]
+    then
+        rm -vf $destination/$file 
+        ln -vs $destination/$repodir/$file $destination/$file
+    elif [[ -d $destination/$(dirname $file) ]]
+    then
+        ln -vs $destination/$repodir/$file $destination/$file
+    else
+        if [[ -d $destination/$(dirname $file) ]]
+        then
+            ln -vs $destination/$repodir/$file $destination/$file
+        else
+            echo "Not sure what to do with $file"
+        fi
+    fi
+done
