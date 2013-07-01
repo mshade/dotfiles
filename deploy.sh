796cc68e1d74d1fc3661132b0656f0a9dbbc846a
Mike Shade <mshade@mshade.org>
2013-06-30 20:03:41 -0400

    tweaked i3 floating behavior

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,25 @@
 #
 # Script to deploy dotfiles repo to ~
 
-
 # Enumerate all tracked files.
-
 files=$(git ls-files)
+repodir="./dotfiles"
+destination=$HOME
+
+# loop over files and create symlinks/directories where necessary
+cd $destination
+
+for file in $files
+do
+    # Test if file exists
+    if [[ -f $file ]]
+    then
+        echo "replacing $file with $repodir/$file"
+        rm $file && ln -s $repodir/$file $file
+    else
+        echo "creating link to $repodir/$file"
+        ln -s $repodir/$file $file
+    fi
+done
+
 
