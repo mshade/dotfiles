beffb84f08d0f51e59fccebceb68f9b1c81263b3
Mike Shade <mshade@mshade.org>
2014-06-10 10:25:47 -0400

    battery detection for conky wrapper

--- bin/bin/i3conky.sh
+++ bin/bin/i3conky.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+BAT=~/.i3/i3conk-battery
+NOBAT=~/.i3/i3conk-nobattery
+
+CFG="$NOBAT"
+
+[[ -e /sys/class/power_supply/BAT0 ]] && CFG="$BAT" 
+
 echo '{"version":1}'
 echo '['
 echo '[],'
-exec conky -c ~/.i3/i3conk 
+exec conky -c $CFG
